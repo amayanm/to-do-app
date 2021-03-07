461d50ce080c746c5a153558c6e5887669123c0f
amayanm <74217770+amayanm@users.noreply.github.com>
2021-03-07 14:58:07 -0500

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,14 @@
+# to-do-app
+
+Track a set of TODO items and mark them as completed.
+
+Built using React and appropriate for mobile devices.
+
+# Features
+    - Add new TODO items
+    - Filter by Completed
+    - Strikethrough on completed items
+
+
+You can find it here:
+https://amayanm.github.io/to-do-app/
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+let todoList = [
+  {
+    id:1,
+    completed:true,
+    description: "Complete a demo of React and Github Pages",
+  },
+  {
+    id:2,
+    completed:true,
+    description: "Demo a TODO list application",
+  },
+]
+
+export default class TodoList extends React.Component{
+  constructor(props){
+    super(props)
+    let inputRef = React.createRef()
+    let checkRef = React.createRef()
+    let filterRef = React.createRef()
+    this.state = {todoList, curId:todoList.length+1, inputRef, checkRef, filterRef, filtered:false}
+  }
+  completeItem = (itemId) =>{
+    this.setState((prevState) =>{
+      let prevList = prevState.todoList
+      let itemIndex = prevList.findIndex((item) => item.id === itemId)
+        prevList[itemIndex] = {
+            ...prevList[itemIndex],
+            completed: !prevList[itemIndex].completed,
+      }
+      return {todoList:[...prevList]}
+    })
+  }
+  handleKeyPress = (event) => {
+    if (event.key === "Enter"){
+      this.addTodoItem()
+    }
+  }
+  addTodoItem = () => {
+    let prevValue = this.state.inputRef.current.value
+    this.setState(prevState => ({
+      todoList:[...prevState.todoList,
+        {completed:false, description: prevValue, id:prevState.curId}],
+        curId:prevState.curId+1
+      }))
+    this.state.inputRef.current.value = ""
+  }
+  render() {
+    let todoList = this.state.todoList
+      if (this.state.filtered) {
+        todoList = todoList.filter(item => !item.completed)
+      }
+
+      todoList = todoList.map((item) =>(
+        <div key={item.id}>
+          <label>
+            <input ref={this.state.checkRef} type="checkbox" onChange={() => this.completeItem(item.id)} defaultChecked={item.completed}></input>
+            <span style={item.completed ? {textDecoration: "line-through"} : undefined}>{item.description}</span>
+          </label>
+        </div>
+    ))
+  return(
+    <>
+      <h1>What to do Today</h1>
+      <label>
+        <input ref={this.state.filterRef} type="checkbox"
+          onChange={() => this.setState((prev) => ({filtered: !prev.filtered}))}
+          defaultChecked={false}></input>
+        Filter completed items
+      </label>
+          <hr></hr>
+      <div style={{padding:"5px"}}>
+        {todoList}
+      </div>
+      <input onKeyPress={(event) => this.handleKeyPress(event)} ref={this.state.inputRef}></input>
+      <button onClick={() => this.addTodoItem()}>Add TODO item</button>
+    </>
+  )
+  }
+}
